31ca930d4c6187c54e03c2f36ec961b3894e9c0b
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-06-13 11:35:38 +0200

    update crm and tei model

--- scripts/tei2cidoc.py
+++ scripts/tei2cidoc.py
@@ -27,7 +27,7 @@ items = doc.any_xpath(".//tei:event")
 # create event types
 assertive = URIRef(f"{AMP}event/types/assertive")
 non_assertive = URIRef(f"{AMP}event/types/non-assertive")
-nano_publication = URIRef(f"{AMP}event/nano-publication")
+nano_publication = URIRef(f"{AMP}event/types/nano-publication")
 g.add((assertive, RDF.type, CIDOC["P2_has_type"]))
 g.add((non_assertive, RDF.type, CIDOC["P2_has_type"]))
 g.add((nano_publication, RDF.type, CIDOC["P2_has_type"]))
@@ -39,55 +39,38 @@ for x in tqdm(items, total=len(items)):
     event_type = x.attrib["type"]
     if event_type == "assertive":
         g.add((subj, RDF.type, assertive))
+        date_from = x.attrib["notBefore"]
+        date_to = x.attrib["notAfter"]
+        g.add((subj, CIDOC["P82a_begin_of_the_begin"], Literal(date_from)))
+        g.add((subj, CIDOC["P82b_end_of_the_end"], Literal(date_to)))
+        corresp = x.attrib["corresp"]
+        g.add((subj, CIDOC["P123_resulted_in"], URIRef(f"{AMP}{corresp.split('#')[-1]}")))
     elif event_type == "non-assertive":
         g.add((subj, RDF.type, non_assertive))
+        # create sub-event properties
+        resp = x.attrib["resp"]
+        cert = x.attrib["cert"]
+        date_from = x.attrib["from"]
+        date_to = x.attrib["to"]
+        g.add((subj, CIDOC["E91_co_reference_assignment"], Literal(cert)))
+        g.add((subj, CIDOC["P1_is_identified_by"], Literal(resp)))
+        g.add((subj, CIDOC["P82a_begin_of_the_begin"], Literal(date_from)))
+        g.add((subj, CIDOC["P82b_end_of_the_end"], Literal(date_to)))
+        source = x.attrib["source"]
+        source_uri = URIRef(f"{AMP}{source.split('https://doi.org/')[-1]}")
+        g.add((source_uri, RDF.type, nano_publication))
+        g.add((source_uri, CIDOC["P70_is_documented_in"], URIRef(source)))
+        g.add((source_uri, CIDOC["P123_resulted_from"], subj))
+        g.add((subj, CIDOC["P123_resulted_in"], source_uri))
+        corresp = x.attrib["corresp"]
+        g.add((subj, CIDOC["P123_resulted_from"], URIRef(f"{AMP}{corresp.split('#')[-1]}")))
     else:
-        g.add((subj, RDF.type, assertive))
+        print("event is not assertive or non-assertive")
     g.add((subj, RDF.type, CIDOC["E5_Event"]))
-    g.add((subj, CIDOC["P1_is_identified_by"], Literal("Grigoriou, Dimitra")))
     label = " ".join(x.xpath("./tei:label//text()", namespaces=doc.nsmap))
     g.add((subj, RDFS.label, Literal(label)))
     desc = " ".join(x.xpath("./tei:desc//text()", namespaces=doc.nsmap))
     g.add((subj, RDFS.comment, Literal(desc)))
-    # check if there is another event inside and create type
-    try:
-        na_event = x.xpath("./tei:event", namespaces=doc.nsmap)[0]
-    except IndexError:
-        na_event = None
-    if na_event is not None:
-        xml_id = na_event.attrib["{http://www.w3.org/XML/1998/namespace}id"]
-        item_id = f"{AMP}{xml_id}"
-        subj2 = URIRef(item_id)
-        g.add((subj2, RDF.type, CIDOC["E5_Event"]))
-        na_event_type = na_event.attrib["type"]
-        if na_event_type == "assertive":
-            g.add((subj2, RDF.type, assertive))
-        elif na_event_type == "non-assertive":
-            g.add((subj2, RDF.type, non_assertive))
-        else:
-            g.add((subj2, RDF.type, assertive))
-        # create sub-event properties
-        resp = na_event.attrib["resp"]
-        cert = na_event.attrib["cert"]
-        date_from = na_event.attrib["from"]
-        date_to = na_event.attrib["to"]
-        label = " ".join(na_event.xpath("./tei:label//text()", namespaces=doc.nsmap))
-        desc = " ".join(na_event.xpath("./tei:desc//text()", namespaces=doc.nsmap))
-        g.add((subj2, CIDOC["P1_is_identified_by"], Literal("Grigoriou, Dimitra")))
-        g.add((subj2, RDFS.label, Literal(label)))
-        g.add((subj2, RDFS.comment, Literal(desc)))
-        g.add((subj2, CIDOC["P14_carried_out_by"], Literal(resp)))
-        g.add((subj2, CIDOC["P82a_begin_of_the_begin"], Literal(date_from)))
-        g.add((subj2, CIDOC["P82b_end_of_the_end"], Literal(date_to)))
-        g.add((subj2, CIDOC["P81a_end_of_the_begin"], Literal(date_from)))
-        g.add((subj2, CIDOC["P81b_begin_of_the_end"], Literal(date_to)))
-        idno = na_event.xpath("./tei:idno//text()", namespaces=doc.nsmap)[0]
-        idno_uri = URIRef(f"{AMP}{idno.split('https://doi.org/')[-1]}")
-        g.add((idno_uri, RDF.type, nano_publication))
-        g.add((idno_uri, CIDOC["P70_is_documented_in"], URIRef(idno)))
-        g.add((subj2, CIDOC["P9_consists_of"], idno_uri))
-        g.add((idno_uri, CIDOC["P123_resulted_from"], subj))
-        g.add((subj, CIDOC["P123_resulted_in"], idno_uri))
 g_all = ConjunctiveGraph(store=store)
 g_all.serialize(destination=os.path.join(rdf_dir, "events.ttl"), format="turtle")
 g_all.serialize(destination=os.path.join(rdf_dir, "events.trig"), format="trig")
